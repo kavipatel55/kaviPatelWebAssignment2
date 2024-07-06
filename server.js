7295b5aaa1b29c53b25793215c40e0e3c6bfa43d
kavipatel55 <kavipatel55@gmail.com>
2024-07-06 18:20:31 -0400

    Set up Express server with MongoDB connection

---
+++ server.js
@@ -0,0 +1,32 @@
+// Import required modules
+const expressInstance = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { error } = require("console");
+
+// Create an instance of Express
+const app = expressInstance();
+
+// Middleware to parse JSON bodies of requests
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://kp:nBSiSLgCx74KuGdB@cluster0.ktw3lwa.mongodb.net/Assignment2?retryWrites=true&w=majority",
+    {}
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Connection failed to MongoDB", err);
+    process.exit(1);
+  });
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running port ${PORT}`);
+});
